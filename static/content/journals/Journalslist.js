6a440bb85389995ca067a50b12983550710b3f1a
Krishna Sarath <sarathdamaraju@hotmail.com>
2020-04-19 15:46:15 +0530

    [chore] updated list and adds UTM params

--- static/content/journals/Journalslist.js
+++ static/content/journals/Journalslist.js
@@ -1,5 +1,10 @@
 let Journalslist = [
   {
+    name: 'Array itertors cheat sheet - 1',
+    internal: false,
+    link: 'https://medium.com/small-things/js-array-iterators-cheat-sheet-part-1-8647e59a14ad/?ref=mywebsite'
+  },
+  {
     name: 'Advice to younger me',
     internal: true,
     link: './journal/advice_to_younger_me'
@@ -7,37 +12,37 @@ let Journalslist = [
   {
     name: 'A complete <dev> guide for better buttons',
     internal: false,
-    link: 'https://codeburst.io/a-complete-dev-guide-for-better-buttons-b2efb773a5ef'
+    link: 'https://codeburst.io/a-complete-dev-guide-for-better-buttons-b2efb773a5ef/?ref=mywebsite'
   },
   {
     name: 'Well we made a tool -- RevealURL and benifited from it',
     internal: false,
-    link: 'https://codeburst.io/well-i-made-a-tool-revealurl-and-i-benefited-from-it-40ad32e799ec'
+    link: 'https://codeburst.io/well-i-made-a-tool-revealurl-and-i-benefited-from-it-40ad32e799ec/?ref=mywebsite'
   },
   {
     name: 'Data Driven SEO for Start-ups',
     internal: false,
-    link: 'https://medium.com/hello-meets/data-driven-seo-for-start-ups-fa926c9040e1',
+    link: 'https://medium.com/hello-meets/data-driven-seo-for-start-ups-fa926c9040e1/?ref=mywebsite'
   },
   {
     name: 'Let the branch name do all the talking in git',
     internal: false,
-    link: 'https://codeburst.io/let-the-branch-name-do-all-the-talking-in-git-e614ff85aa30',
+    link: 'https://codeburst.io/let-the-branch-name-do-all-the-talking-in-git-e614ff85aa30/?ref=mywebsite',
   },
   {
     name: 'No-Redux Strategy for Siblings communication',
     internal: false,
-    link: 'https://codeburst.io/no-redux-strategy-for-siblings-communication-3db543538959',
+    link: 'https://codeburst.io/no-redux-strategy-for-siblings-communication-3db543538959/?ref=mywebsite',
   },
   {
     name: 'git — Rename and Delete Branches',
     internal: false,
-    link: 'https://medium.com/small-things/git-rename-and-delete-branches-54fa10444932',
+    link: 'https://medium.com/small-things/git-rename-and-delete-branches-54fa10444932/?ref=mywebsite',
   },
   {
     name: '5 steps to SetUp SASS with Grunt and NodeJS for Beginners',
     internal: false,
-    link: 'https://medium.com/small-things/5-steps-to-setup-sass-with-grunt-and-nodejs-for-beginners-e6d7512fcb7b',
+    link: 'https://medium.com/small-things/5-steps-to-setup-sass-with-grunt-and-nodejs-for-beginners-e6d7512fcb7b/?ref=mywebsite',
   },
   {
     name: 'Git Aliases That Could Be Helpful',
