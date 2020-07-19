84d4f38421a54bdf6233a89df6353ff2721389a9
Krishna Sarath <sarathdamaraju@hotmail.com>
2020-07-19 18:29:34 +0530

    refactor: :lipstick: Changes theme and code refactor (#27)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,8 @@ import Link from 'next/link';
 
 const Footer = () => (
   <footer className="position--fixed">
-    made with{' '}
+    made with
+    {' '}
     <Link href="#">
       <a>nextjs</a>
     </Link>
